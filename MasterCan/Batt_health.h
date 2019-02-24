5e617729b8c2c1d9a232e079b3b0d990c18c3ac4
DanieleVBella <danielebella2000@gmail.com>
2019-02-24 13:45:05 +0000

    Removed useless variable in batt health library to save space

--- MasterCan/Batt_health.h
+++ MasterCan/Batt_health.h
@@ -20,13 +20,11 @@ class batt {
     // Function that outputs the battery health as a percent
     float percent(int battHealthPin) {
 
-      // Read the analog voltage and convert to percent
-      battPercent = analogRead(battHealthPin) * (100. / 9.) * (5. / 1023.) * (11);
+      // Return the analog voltage and convert to percent
+      return analogRead(battHealthPin) * (100. / 9.) * (5. / 1023.) * (11);
       // 5/1023 due to arduino analog scaling
       //11 due to reciprocal of 1/11 from Vout = R2/(R1+R2)*Vin
       //100%9 to calculate percentage based on 9V initial health
-
-      return battPercent;
 
     }
 };
